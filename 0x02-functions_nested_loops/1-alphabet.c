c2177cdd20c2ea23fd719acc66e61e55db0134e2
saidimt <mtembezi2020@gmail.com>
2022-06-22 10:57:49 -0700

    task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *
+ *  * print_alphabet - Entry point
+ *
+ *   * Description: a function that prints the alphabet,
+ *
+ *    * in lowecase followed by a new line
+ *
+ *     * Return: void
+ *
+ *      */
+void print_alphabet(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+_putchar(c);
+_putchar('\n');
+}
