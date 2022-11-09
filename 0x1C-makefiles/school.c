d65c86f651ce0204f142745f145404fbdbe4b25a
saidimt <mtembezi2020@gmail.com>
2022-11-09 09:27:19 -0800

    About

---
+++ 0x1C-makefiles/school.c
@@ -0,0 +1,21 @@
+#include "m.h"
+
+
+
+/**
+ *
+ *  * print_holberton - print the logo of Holberton
+ *
+ *   *
+ *
+ *    * Return: Nothing
+ *
+ *     *
+ *
+ *      * Learn more: https://www.holbertonschool.com
+ *
+ *       */
+
+void print_holberton(void)
+
+{
